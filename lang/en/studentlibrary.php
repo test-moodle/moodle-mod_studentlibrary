8a0a0b6ace37a1217cff2b34f0f48e6e750af61c
ShekhovtcevD <shekhovtcev@mgupp.ru>
2025-01-22 17:26:33 +0300

    fix  Licensing

--- lang/en/studentlibrary.php
+++ lang/en/studentlibrary.php
@@ -1,12 +1,27 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Plugin strings are defined here.
+ * Plugin administration pages are defined here.
  *
  * @package     mod_studentlibrary
- * @category    string
- * @copyright   2020 itsup.biz
-
+ * @category    admin
+ * @copyright   2025 shekhovtcev <plagin@geotar.ru>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
