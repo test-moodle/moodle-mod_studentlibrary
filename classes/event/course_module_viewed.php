1a683e79e28e888181cd223f1652ccefcf728042
ShekhovtcevD <shekhovtcev@mgupp.ru>
2025-01-23 15:04:38 +0300

    fix course_module_viewed.php

--- classes/event/course_module_viewed.php
+++ classes/event/course_module_viewed.php
@@ -19,7 +19,7 @@
  *
  * @package     mod_studentlibrary
  * @category    admin
- * @copyright   2025 <plagin@geotar.ru>
+ * @copyright   2025 shekhovtcev <plagin@geotar.ru>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -27,30 +27,15 @@ namespace mod_studentlibrary\event;
 
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * The course_module_viewed event class.
- *
- * @package    mod_test
- * @copyright  2020 itsup.biz
- */
- 
 class course_module_viewed extends \core\event\course_module_viewed {
-
     // For more information about the Events API, please visit:
     // https://docs.moodle.org/dev/Event_2
-	    /**
-     * Init method.
-     *
-     * @return void
-     */
     protected function init() {
         $this->data['objecttable'] = 'studentlibrary';
         $this->data['crud'] = 'r';
         $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
     }
-
     public static function get_objectid_mapping() {
-        return array('db' => 'studentlibrary', 'restore' => 'studentlibrary');
+        return (['db' => 'studentlibrary', 'restore' => 'studentlibrary']);
     }
-
 }
