1625243bd3d9cd93bf89effa95c11f4f576ce34a
ShekhovtcevD <shekhovtcev@mgupp.ru>
2025-01-23 14:26:33 +0300

    mod_form.php

--- mod_form.php
+++ mod_form.php
@@ -31,44 +31,32 @@ require_once($CFG->dirroot . '/course/moodleform_mod.php');
 require_once(__DIR__ . '/lib.php');
 require_once(__DIR__ . '/locallib.php');
 /**
-* Module instance settings form.
-*
-* @package    mod_studentlibrary
-* @copyright  2020 itsup.biz
-* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-*/
-class mod_studentlibrary_mod_form extends moodleform_mod
-{
-    /**
-    * Defines forms elements
-    */
+ * Module instance settings form.
+ *
+ * @package    mod_studentlibrary
+ * @copyright  2020 itsup.biz
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_studentlibrary_mod_form extends moodleform_mod {
+    // Defines forms elements
     public function definition()
     {
         global $CFG, $DB, $PAGE, $USER, $SESSION;
         $serverapi = get_mod_config('serverapi');
-        $orgid = $DB->get_record('config', array('name' => 'studentlibrary_idorg'))->value;
-        $agrid = $DB->get_record('config', array('name' => 'studentlibrary_norg'))->value;
+        $orgid = $DB->get_record('config', ['name' => 'studentlibrary_idorg'])->value;
+        $agrid = $DB->get_record('config', ['name' => 'studentlibrary_norg'])->value;
         if (substr($serverapi, -1) !== '/') {
             $serverapi = $serverapi . '/';
         }
-        /**
-         * Получаем сессию организации
-         * We get the organization's session
-         */
+        // We get the organization's session. Получаем сессию организации.
         $ssro = getSSr_O($serverapi, $orgid, $agrid);
-        /**
-         * Получаем сессию пользователя
-         * Getting the user's session
-         */
+        // Getting the user's session. Получаем сессию пользователя.
         $ssrp = getSSr_P($serverapi, $ssro, $USER->id, str_replace(' ', '_', $USER->lastname), str_replace(' ', '_', $USER->firstname));
-        /**
-         * Получаем комплект книг
-         * We get a set of books
-         */
+        // We get a set of books. Получаем набор книг.
         $kitsList = getKitsList($serverapi, $ssrp);
         $mform = $this->_form;
         $mform->addElement('header', 'general', get_string('general', 'form'));
-        $mform->addElement('text', 'name', "Название", array('size' => '64'));
+        $mform->addElement('text', 'name', "Название", ['size' => '64']);
         if (!empty($CFG->formatstringstriptags)) {
             $mform->setType('name', PARAM_TEXT);
         } else {
@@ -84,8 +72,8 @@ class mod_studentlibrary_mod_form extends moodleform_mod
         }
         $PAGE->requires->css('/mod/studentlibrary/css/style.css');
         if (!empty($SESSION->lang)) {
-            if($SESSION->lang !== null) {
-                $lang =$SESSION->lang;
+            if ($SESSION->lang !== null) {
+                $lang = $SESSION->lang;
             } else {
                 $lang = 'ru';
             }
@@ -101,7 +89,7 @@ class mod_studentlibrary_mod_form extends moodleform_mod
             get_string('studentlibrary:add_material', 'mod_studentlibrary'),
             get_string('studentlibrary:search_bar', 'mod_studentlibrary'),
             get_string('studentlibrary:link_to_the_kit', 'mod_studentlibrary'),
-            $lang
+            $lang,
         ]);
         $courseid = optional_param('course', 0, PARAM_INT);
         $section = optional_param('section', 0, PARAM_INT);
@@ -110,7 +98,7 @@ class mod_studentlibrary_mod_form extends moodleform_mod
                 'button',
                 'search_button',
                 get_string('studentlibrary:search_button', 'mod_studentlibrary'),
-                array(
+                [
                     'id' => 'search_button',
                     'data-courseid' => $courseid,
                     'data-token' => '',
@@ -118,28 +106,27 @@ class mod_studentlibrary_mod_form extends moodleform_mod
                     'data-service' => '',
                     'ssr_o' => $ssro,
                     'ssr_p' => $ssrp,
-                    'kitsList' => implode(",", $kitsList)
-                    )
-                );
-            }
-            $mform->addElement(
-                'text',
-                'booke',
-                get_string('studentlibrary:bookid', 'mod_studentlibrary'),
-                array(
-                    'size' => '64',
-                    'ssr_o' => $ssro,
-                    'ssr_p' => $ssrp,
-                    )
-                );
-                if (!empty($CFG->formatstringstriptags)) {
-                    $mform->setType('booke', PARAM_TEXT);
-                } else {
-                    $mform->setType('booke', PARAM_CLEANHTML);
-                }
-                $mform->addElement('html', '<div><a target="_blank" href="https://www.studentlibrary.ru/ru/pages/plagin.html">' . get_string('studentlibrary:instruction', 'mod_studentlibrary') . '</a></div>');
-                $this->standard_coursemodule_elements();
-                $this->add_action_buttons();
-            }
+                    'kitsList' => implode(",", $kitsList),
+                ]
+            );
         }
-        
+        $mform->addElement(
+            'text',
+            'booke',
+            get_string('studentlibrary:bookid', 'mod_studentlibrary'),
+            [
+                'size' => '64',
+                'ssr_o' => $ssro,
+                'ssr_p' => $ssrp,
+            ]
+        );
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('booke', PARAM_TEXT);
+        } else {
+            $mform->setType('booke', PARAM_CLEANHTML);
+        }
+        $mform->addElement('html', '<div><a target="_blank" href="https://www.studentlibrary.ru/ru/pages/plagin.html">' . get_string('studentlibrary:instruction', 'mod_studentlibrary') . '</a></div>');
+        $this->standard_coursemodule_elements();
+        $this->add_action_buttons();
+    }
+}
