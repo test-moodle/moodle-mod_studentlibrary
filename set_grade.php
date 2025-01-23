b1c3da1a7c79f6823b52b88fbe2a7279959e3ccb
ShekhovtcevD <shekhovtcev@mgupp.ru>
2025-01-23 11:57:48 +0300

    fix set_grade.php

--- set_grade.php
+++ set_grade.php
@@ -23,35 +23,32 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Получение данных
-require(__DIR__.'/../../config.php');
+// Get data. Получение данных.
+require(__DIR__ . '/../../config.php');
 global $DB, $CFG;
 
-if (isset($_GET['apikey'])){
-    /** 
-     * Проверяю что ключ есть в базе
-     * I'm checking that the key is in the database.
-     * */
-    $apikey_exists = $DB->record_exists('studentlibrary_apikey', array('apikey' => $_GET['apikey']));
-    if ($apikey_exists){
-        $apikey_data = $DB->get_record('studentlibrary_apikey', array('apikey' => $_GET['apikey']), '*', MUST_EXIST);
+if (isset($_GET['apikey'])) {
+    // Checking that the key is in the database. Проверяю что ключ есть в базе.
+    $apikeyexists = $DB->record_exists('studentlibrary_apikey', ['apikey' => $_GET['apikey']]);
+    if ($apikeyexists) {
+        $apikeydata = $DB->get_record('studentlibrary_apikey', ['apikey' => $_GET['apikey']], '*', MUST_EXIST);
         $result = new stdClass();
-        $result->course=$apikey_data->course;
-        $result->module=$apikey_data->module;
-        $result->userid=$apikey_data->user;
-        $result->total=$_GET['total'];
-        $result->score=$_GET['score'];
-        $result->report=$_GET['report'];
-        $result->modified=time();
-        $DB->delete_records('studentlibrary_apikey', array('apikey' => $_GET['apikey']), '*', MUST_EXIST);
-        print_r('{"status":"ok"}');
-        $urltogo= $CFG->wwwroot.'/mod/studentlibrary/view.php?id='.$apikey_data->module;
+        $result->course = $apikeydata->course;
+        $result->module = $apikeydata->module;
+        $result->userid = $apikeydata->user;
+        $result->total = $_GET['total'];
+        $result->score = $_GET['score'];
+        $result->report = $_GET['report'];
+        $result->modified = time();
+        $DB->delete_records('studentlibrary_apikey', ['apikey' => $_GET['apikey']], '*', MUST_EXIST);
+        echo('{"status":"ok"}');
+        $urltogo = $CFG->wwwroot . '/mod/studentlibrary/view.php?id=' . $apikeydata->module;
         redirect($urltogo);
-    }else{
-        $urltogo= $CFG->wwwroot.'/';
+    } else {
+        $urltogo = $CFG->wwwroot . '/';
         redirect($urltogo);
     }
-}else{
-    $urltogo= $CFG->wwwroot.'/';
+} else {
+    $urltogo = $CFG->wwwroot . '/';
     redirect($urltogo);
 }
